86c3db9a9feb5bc1728c3928efffda3cbc715b20
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-01-17 00:55:07 +0000

    fix - correct variable name and conditional structure

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 $featuresDir = "features";
 $fidoDir = "FIDO";
 $pagesDir = "pages";
-$templates = "templates";
+$templatesDir = "templates";
 
 function read($dir)
 {
@@ -12,7 +12,7 @@ function read($dir)
     while ($file = readdir($handle)) {
         if (is_dir($dir . "/" . $file) && !in_array($file, $ignorePaths)) {
             echo "<li class='list-group-item'><a href='" . $dir . "/" . $file . "'>" . preg_replace('/(?<!^)([A-Z])/', ' \\1', $file) . "</a></li>\r\n";
-        } else if (is_file($dir . "/" . $file) && !in_array($file, $ignorePaths)) {
+        } elseif (is_file($dir . "/" . $file) && !in_array($file, $ignorePaths)) {
             echo "<li class='list-group-item'><a href='" . $dir . "/" . $file . "'>" . $file . "</a></li>\r\n";
         }
     }
